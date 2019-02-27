939f85595eca28c07b3b4d0191e60203c5553ec8
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-02-27 16:26:15 +0900

    fix:デザイン修正による処理の修正

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,14 @@ import Amplify, * as AmplifyModules from "aws-amplify";
 import { AmplifyPlugin } from "aws-amplify-vue";
 import aws_exports from "./aws-exports";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(fas, far, fab);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
 Amplify.configure(aws_exports);
 Vue.use(AmplifyPlugin, AmplifyModules);
 
