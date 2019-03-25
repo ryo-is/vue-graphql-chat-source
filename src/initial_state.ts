76ca07f98053138e2548a58e25347f7d8c69a5ee
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-03-25 15:42:08 +0900

    fix:UserデータをStoreへの持たせ方を修正

--- src/initial_state.ts
+++ src/initial_state.ts
@@ -1,19 +1,15 @@
+import { ChatUsersType } from "@/interfaces";
+
 interface initialStateType {
   userID: string;
   displayName: string;
-  todo_task_ids: string[];
-  doing_task_ids: string[];
-  check_task_ids: string[];
-  done_task_ids: string[];
+  user: ChatUsersType | null;
 }
 
 const initialState: initialStateType = {
   userID: "",
   displayName: "",
-  todo_task_ids: [],
-  doing_task_ids: [],
-  check_task_ids: [],
-  done_task_ids: []
+  user: null
 };
 
 export default initialState;
