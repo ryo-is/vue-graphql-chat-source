ddddbe90532092a95536dc7b4c7f428ebb744535
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-03-27 14:55:05 +0900

    fix:TaskのStatusが前後しないように修正

--- src/views/todo/todo.ts
+++ src/views/todo/todo.ts
@@ -55,7 +55,11 @@ export default class TodoComponent extends Vue {
       `;
       const result: any = await API.graphql(graphqlOperation(params));
       if (result.data.getTaskContents !== null) {
-        this.todoTasks = JSON.parse(result.data.getTaskContents.tasks);
+        const taskContents: {[key: string]: TaskType[]} = JSON.parse(result.data.getTaskContents.tasks);
+        Object.keys(this.todoTasks).forEach((key: string) => {
+          this.todoTasks[key] = taskContents[key];
+        });
+        console.log(this.todoTasks);
       } else {
         this.createTask();
       }
